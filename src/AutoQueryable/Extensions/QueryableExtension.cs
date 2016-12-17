4e7ff1619ee5cebcca3390392cbc0df6303f72ef
trenoncourt <thibaut.renoncourt@gmail.com>
2016-12-17 22:20:26 +0100

    Add AutoQueryable extension on top of Iqueryable<T>

---
+++ src/AutoQueryable/Extensions/QueryableExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using AutoQueryable.Helpers;
+using AutoQueryable.Models;
+
+namespace AutoQueryable.Extensions
+{
+    public static class QueryableExtension
+    {
+        public static dynamic AutoQueryable(this IQueryable<object> query, string queryString, AutoQueryableProfile profile)
+        {
+            Type entityType = query.GetType().GenericTypeArguments[0];
+            return QueryableHelper.GetAutoQuery(queryString, entityType, query, profile);
+        }
+    }
+}
