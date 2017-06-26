6c141aab1e8c27b4bd99ec5054d72cc72b9f62ce
Thibaut RENONCOURT <trenoncourt@access-it.fr>
2017-06-26 12:28:06 +0200

    refacto

--- src/AutoQueryable/Helpers/QueryableHelper.cs
+++ src/AutoQueryable/Helpers/QueryableHelper.cs
@@ -32,47 +32,7 @@ namespace AutoQueryable.Helpers
             }
 
             IEnumerable<WrapperPartType> wrapperParts = WrapperManager.GetWrapperParts(wrapWithClause.Value.Split(',')).ToList();
-            dynamic wrapper = new ExpandoObject();
-            wrapper.Result = (result as IQueryable<object>).ToList();
-            foreach (var part in wrapperParts)
-            {
-                switch (part)
-                {
-                    case WrapperPartType.Count:
-                        bool isResultEnumerableCount = typeof(IEnumerable).IsAssignableFrom((Type)result.GetType());
-                        if (isResultEnumerableCount)
-                        {
-                            wrapper.Count = wrapper.Result.Count;
-                        }
-                        break;
-                    case WrapperPartType.NextLink:
-                        bool isResultEnumerableNextLink = typeof(IEnumerable).IsAssignableFrom((Type)result.GetType());
-
-                        Clause topClause = clauses.FirstOrDefault(c => c.ClauseType == ClauseType.Top);
-                        Clause skipClause = clauses.FirstOrDefault(c => c.ClauseType == ClauseType.Skip);
-                        if (isResultEnumerableNextLink && topClause != null)
-                        {
-                            int skip = skipClause == null ? 0 : Convert.ToInt32(skipClause.Value);
-                            int take = Convert.ToInt32(topClause.Value);
-                            skip += take;
-                            if (wrapper.Result.Count < take)
-                            {
-                                break;
-                            }
-                            if (skipClause != null)
-                            {
-                                wrapper.NextLink = queryString.ToLower().Replace($"{ClauseAlias.Skip}{skipClause.Value}", $"{ClauseAlias.Skip}{skip}");
-                            }
-                            else
-                            {
-                                wrapper.NextLink = $"{queryString.ToLower()}&{ClauseAlias.Skip}{skip}";
-                            }
-                            
-                        }
-                        break;
-                }
-            }
-            return wrapper;
+            return WrapperManager.GetWrappedResult(wrapperParts, result, clauses, queryString);
         }
     }
 }
--- src/AutoQueryable/Managers/WrapperManager.cs
+++ src/AutoQueryable/Managers/WrapperManager.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using AutoQueryable.Aliases;
 using AutoQueryable.Models;
 using AutoQueryable.Extensions;
+using System.Dynamic;
+using System.Linq;
+using System.Collections;
+using System.Reflection;
 
 namespace AutoQueryable.Managers
 {
@@ -21,6 +25,51 @@ namespace AutoQueryable.Managers
                     yield return WrapperPartType.NextLink;
                 }
             }
+        }
+
+        public static dynamic GetWrappedResult(IEnumerable<WrapperPartType> wrapperParts, dynamic result, IList<Clause> clauses, string queryString)
+        {
+            dynamic wrapper = new ExpandoObject();
+            wrapper.Result = (result as IQueryable<object>).ToList();
+            foreach (var part in wrapperParts)
+            {
+                switch (part)
+                {
+                    case WrapperPartType.Count:
+                        bool isResultEnumerableCount = typeof(IEnumerable).IsAssignableFrom((Type)result.GetType());
+                        if (isResultEnumerableCount)
+                        {
+                            wrapper.Count = wrapper.Result.Count;
+                        }
+                        break;
+                    case WrapperPartType.NextLink:
+                        bool isResultEnumerableNextLink = typeof(IEnumerable).IsAssignableFrom((Type)result.GetType());
+
+                        Clause topClause = clauses.FirstOrDefault(c => c.ClauseType == ClauseType.Top);
+                        Clause skipClause = clauses.FirstOrDefault(c => c.ClauseType == ClauseType.Skip);
+                        if (isResultEnumerableNextLink && topClause != null)
+                        {
+                            int skip = skipClause == null ? 0 : Convert.ToInt32(skipClause.Value);
+                            int take = Convert.ToInt32(topClause.Value);
+                            skip += take;
+                            if (wrapper.Result.Count < take)
+                            {
+                                break;
+                            }
+                            if (skipClause != null)
+                            {
+                                wrapper.NextLink = queryString.ToLower().Replace($"{ClauseAlias.Skip}{skipClause.Value}", $"{ClauseAlias.Skip}{skip}");
+                            }
+                            else
+                            {
+                                wrapper.NextLink = $"{queryString.ToLower()}&{ClauseAlias.Skip}{skip}";
+                            }
+
+                        }
+                        break;
+                }
+            }
+            return wrapper;
         }
     }
 }
