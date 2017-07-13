9af70c8f64e01b12d6deb768204764a9a4ba0424
Nurhat Uca <nurhat.uca@gmail.com>
2017-07-13 09:50:19 +0300

    fix typekey name => Type name was too long. The fully qualified type name must be less than 1,024 characters

--- src/AutoQueryable/Helpers/SelectHelper.cs
+++ src/AutoQueryable/Helpers/SelectHelper.cs
@@ -7,6 +7,8 @@ using AutoQueryable.Extensions;
 using AutoQueryable.Models;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace AutoQueryable.Helpers
 {
@@ -75,7 +77,18 @@ namespace AutoQueryable.Helpers
                 }
                 return current;
             });
-            return typeof(TEntity).FullName + fieldsKey;
+            return  typeof(TEntity).FullName + getHash(fieldsKey);
+        }
+        private static string getHash(string text)
+        {
+            // SHA512 is disposable by inheritance.  
+            using (var sha256 = SHA256.Create())
+            {
+                // Send a sample text to hash.  
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+                // Get the hashed string.  
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
         }
     }
 
