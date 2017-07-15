a291311c10c666452b1de71e4a8fdb8fa765d472
trenoncourt <thibaut.renoncourt@gmail.com>
2017-07-15 10:55:00 +0200

    Remove Include from clause types.

--- src/AutoQueryable/Models/Enums/ClauseType.cs
+++ src/AutoQueryable/Models/Enums/ClauseType.cs
@@ -11,7 +11,6 @@ namespace AutoQueryable.Models.Enums
         Skip = 1 << 4,
         OrderBy = 1 << 5,
         OrderByDesc = 1 << 6,
-        Include = 1 << 7,
         GroupBy = 1 << 8,
         First = 1 << 9,
         Last = 1 << 10,
