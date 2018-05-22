43c0d92af8b857b413cd345fe740ede22bed4f79
Thibaut Renoncourt <thibaut.renoncourt@gmail.com>
2018-05-22 10:37:53 +0200

    feat(swagger): initialize swagger support for AQ

---
+++ src/AutoQueryable.AspNetCore.Swagger/AutoQueryableOperationFilter.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using AutoQueryable.AspNetCore.Filter.FilterAttributes;
+using Microsoft.AspNetCore.Authorization;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace AutoQueryable.AspNetCore.Swagger
+{
+    public class AutoQueryableOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            // Policy names map to scopes
+            var controllerScopes = context.ApiDescription.ActionAttributes()
+                .OfType<AutoQueryableAttribute>();
+
+            if (controllerScopes.Any())
+            {
+                Schema schema = context.SchemaRegistry.GetOrRegister(typeof(string));
+                operation.Parameters.Add(new NonBodyParameter
+                {
+                    In = "query",
+                    Name = "select",
+                    Type = schema.Type,
+                    Format = schema.Format
+                });
+            }
+        }
+    }
+}
---
+++ src/AutoQueryable.AspNetCore.Swagger/SwaggerGenOptionsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace AutoQueryable.AspNetCore.Swagger
+{
+    public static class SwaggerGenOptionsExtensions
+    {
+        public static void AddAutoQueryable(this SwaggerGenOptions swaggerGenOptions)
+        {
+            swaggerGenOptions.OperationFilter<AutoQueryableOperationFilter>();
+        }
+    }
+}
