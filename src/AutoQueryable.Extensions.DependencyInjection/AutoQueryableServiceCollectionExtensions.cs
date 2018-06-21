d8b3c13ba0d3a1486c9d373d8feb463874d9f52b
Thibaut Renoncourt <thibaut.renoncourt@gmail.com>
2018-06-21 21:07:31 +0200

    feat(di): add dependency injection for net(standard/core)app
    add extension for IServiceCollection to register all needed types.
    AQprofile can be directly initialize in this method

---
+++ src/AutoQueryable.Extensions.DependencyInjection/AutoQueryableServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AutoQueryable.Core.Clauses;
+using AutoQueryable.Core.Clauses.ClauseHandlers;
+using AutoQueryable.Core.CriteriaFilters;
+using AutoQueryable.Core.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutoQueryable.Extensions.DependencyInjection
+{
+    /// <summary>
+    ///     Extension methods for setting up AutoQueryable Framework related services in an <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" />.
+    /// </summary>
+    public static class AutoQueryableServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the minimum essential AutoQueryable services to the specified <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" />. Additional services
+        /// including default clause handlers.
+        /// </summary>
+        /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add services to.</param>
+        /// <param name="handler">An <see cref="T:System.Action`1" /> to configure the provided <see cref="T:AutoQueryable.Core.Models.AutoQueryableProfile" />.</param>
+        /// <returns>An <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> that can be used to further other services.</returns>
+        public static IServiceCollection AddAutoQueryable(this IServiceCollection services, Action<AutoQueryableProfile> handler = null)
+        {
+            var profile = new AutoQueryableProfile();
+            handler?.Invoke(profile);
+            services.AddScoped<IAutoQueryableContext, AutoQueryableContext>();
+            services.AddScoped<IAutoQueryableProfile, AutoQueryableProfile>(_ => profile);
+            services.AddScoped<IAutoQueryHandler, AutoQueryHandler>();
+            services.AddScoped<IClauseValueManager, ClauseValueManager>();
+            services.AddScoped<ICriteriaFilterManager, CriteriaFilterManager>();
+            services.AddScoped<IClauseMapManager, ClauseMapManager>();
+            services.AddScoped<ISelectClauseHandler, DefaultSelectClauseHandler>();
+            services.AddScoped<IOrderByClauseHandler, DefaultOrderByClauseHandler>();
+            services.AddScoped<IWrapWithClauseHandler, DefaultWrapWithClauseHandler>();
+            return services;
+        }
+    }
+}
