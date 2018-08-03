de07652cef7891f2f71afe236ca9693620e3f94a
Thibaut Renoncourt <thibaut.renoncourt@gmail.com>
2018-08-03 08:49:32 +0200

    package: update version to 2.0.3-beta

--- build.bat
+++ build.bat
@@ -5,10 +5,11 @@ dotnet restore
 dotnet build
 
 dotnet pack ".\src\AutoQueryable" -c Release -o ".\bin\NuGetPackages"
-dotnet pack ".\src\AutoQueryable.Core" -c Release -o ".\bin\NuGetPackages"
-dotnet pack ".\src\AutoQueryable.Providers.OData" -c Release -o ".\bin\NuGetPackages"
+dotnet pack ".\src\AutoQueryable.AspNet" -c Release -o ".\bin\NuGetPackages"
 dotnet pack ".\src\AutoQueryable.AspNet.Filter" -c Release -o ".\bin\NuGetPackages"
 dotnet pack ".\src\AutoQueryable.AspNetCore.Filter" -c Release -o ".\bin\NuGetPackages"
-dotnet pack ".\src\AutoQueryable.Nancy.Filter" -c Release -o ".\bin\NuGetPackages"
+dotnet pack ".\src\AutoQueryable.AspNetCore.Swagger" -c Release -o ".\bin\NuGetPackages"
+dotnet pack ".\src\AutoQueryable.Extensions.Autofac" -c Release -o ".\bin\NuGetPackages"
+dotnet pack ".\src\AutoQueryable.Extensions.DependencyInjection" -c Release -o ".\bin\NuGetPackages"
 
 pause
